346f543fe8fb4f6f139807589cbd76d417e2df93
Rozita Teymourzadeh <rteymourzadeh@dons.usfca.edu>
2018-10-05 12:54:28 -0700

    initial implementation of QuickSortDualPivot

---
+++ cs673/src/QuckSortDualPivot.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package src;
+
+/**
+ * @author rozitateymourzadeh
+ *
+ */
+public class QuckSortDualPivot {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void quickSortDualPointer(int[] array, int low, int high) {
+		
+		int pivotIndex = partition(array, low, high);
+		quickSortDualPointer(array, low, pivotIndex - 1);
+		quickSortDualPointer(array, pivotIndex + 1 , high);
+	}
+	
+	public int partition(int[] array, int low, int high) {
+		int pivotIndex = 0;
+		// to be implemented
+		return pivotIndex;
+	}
+
+}
